0ac3f8f662afbf37daec9f7204da1d9da8a853ed
euichan0927 <yuichan99@naver.com>
2023-12-31 00:53:19 +0900

    4153 PS commit

---
+++ baekjoon/class/level2/4153.py
@@ -0,0 +1,21 @@
+import sys,math
+input=sys.stdin.readline
+
+
+while(1):
+    nlist=list(map(int,input().split()))
+    nlist.sort()
+    if(nlist[0]==0 and nlist[1]==0 and nlist[2]==0):
+        break
+    nmax=max(nlist)
+    nlist.remove(nmax)
+    print(nlist[0])
+    print(nlist[1])
+    print(nlist[0]**2)
+   
+
+    if(nmax**2 == ((nlist[0]**2) + (nlist[1]**2))):
+        print("right")
+    else:
+        print("wrong")
+    
