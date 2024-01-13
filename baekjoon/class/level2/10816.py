b6afa2bb73aadf518547857ca95fa5f4413fc3e6
euichan0927 <yuichan99@naver.com>
2024-01-13 23:20:14 +0900

    PS/10816 commit

---
+++ baekjoon/class/level2/10816.py
@@ -0,0 +1,17 @@
+
+from collections import Counter
+n=int(input())
+nlist=list(map(int,input().split()))
+m=int(input())
+mlist=list(map(int,input().split()))
+
+nlist.sort()
+c=Counter(nlist)
+for i in range(m):
+    if(mlist[i] in c):
+        print(c[mlist[i]], end=' ')
+    else:
+        print(0,end=' ')
+
+
+print(c)
