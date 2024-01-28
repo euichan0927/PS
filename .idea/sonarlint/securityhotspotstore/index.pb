14d62cf28be83603fa2139909150b1e0ae5be93f
euichan0927 <yuichan99@naver.com>
2024-01-28 22:33:51 +0900

    PS/11723.py commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+M
+baekjoon/class/level2/1654.py,1\7\173daf55f903652a36dc7717e71a7c5daea73264
---
+++ baekjoon/class/level3/11723.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+all_set=set([str(i) for i in range(1,21)])
+nlist=set()
+m=int(input())
+for i in range(m):
+    command=(input().split())
+    if("add" in command):
+        nlist.add(command[1])
+    elif("remove" in command):
+        nlist.discard(command[1])
+    elif("check" in command):
+        if(command[1] in nlist):
+            print(1)
+        else:
+            print(0)
+    elif("toggle" in command):
+        if(command[1] in nlist):
+            nlist.discard(command[1])
+        else:
+            nlist.add(command[1])
+    elif("all" in command):
+        nlist==all_set.copy()
+        
+    elif("empty" in command):
+        nlist=set()
+        
